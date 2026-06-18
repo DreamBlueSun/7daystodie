a5255525456ff486a4f6a170a6d3795cb7108a4e
DreamBlueSun <271221198@qq.com>
2026-06-18 18:42:07 +0800

    Java-Build

--- iridescent/src/main/java/ume/marisa/iridescent/utils/WriteToFile.java
+++ iridescent/src/main/java/ume/marisa/iridescent/utils/WriteToFile.java
@@ -1,13 +1,18 @@
 package ume.marisa.iridescent.utils;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class WriteToFile {
 
+    private static final String OUTPUT_DIR = "iridescent/src/output";
+
     public static void output(String content) {
-        String filePath = "D:\\idea-workspace-ume\\7daystodie\\iridescent\\src\\output\\output.txt";
+        File dir = new File(OUTPUT_DIR);
+        if (!dir.exists()) dir.mkdirs();
+        String filePath = OUTPUT_DIR + "/output.txt";
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
             writer.write(content);
         } catch (IOException e) {
